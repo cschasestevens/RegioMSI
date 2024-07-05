ce447496bbbf9fae08f10eed2188a8298d08cab3
cschasestevens <chase@havensight.net>
2024-07-05 15:13:24 -0400

    RegioMSI v1.1 Resolved issues with setCardinalNumBlocks deprecation and updated Cardinal objects

--- R/2.2a.process.peaks.detection.R
+++ R/2.2a.process.peaks.detection.R
@@ -1,6 +1,6 @@
-#' Combined Peak Detection for MSI Datasets
+#' Combined Peak Processing for MSI Datasets
 #'
-#' Performs peak detection based on all data files included in MSI.LoadAll() using Cardinal.
+#' Performs peak picking and alignment based on all data files included in MSI.LoadAll() using Cardinal.
 #'
 #' @param msi.list A list of Cardinal MSImagingExperiment objects created from MSI.LoadAll().
 #' @param p.list A list containing input parameters and file names created from MSI.InputParams()
@@ -8,10 +8,10 @@
 #' @param core.perc Percentage of cores to be used during processing.
 #' @return A list containing a data frame of processed peaks and processing details.
 #' @examples
-#' MSI.detectpeaks(list.d[["Data.files"]],list.p,TRUE,0.75)
+#' MSI.processpeaks(list.d[["Data.files"]],list.p,TRUE,0.75)
 #'
 #' @export
-MSI.detectpeaks <- function(
+MSI.processpeaks <- function(
     msi.list,
     p.list,
     parl,
@@ -51,86 +51,7 @@ MSI.detectpeaks <- function(
     )
   }
 
-  # Processing parameters
-
-  # ifelse(
-  #   sum(
-  #     unlist(
-  #       lapply(
-  #         list.d,
-  #         function(x) nrow(
-  #           Cardinal::coord(x)
-  #         )
-  #       )
-  #     )
-  #   )/
-  #     100 <
-  #     2000,
-  #   n.chunk <- 100,
-  #   ifelse(
-  #     sum(
-  #       unlist(
-  #         lapply(
-  #           list.d,
-  #           function(x)
-  #             nrow(
-  #               Cardinal::coord(x)
-  #             )
-  #         )
-  #       )
-  #     )/
-  #       1000 <
-  #       2000,
-  #     n.chunk <- 1000,
-  #     n.chunk <- 10000
-  #   )
-  # )
-
-# if(unlist(packageVersion("Cardinal"))[2] < 6) {
-#     Cardinal::setCardinalNumBlocks(
-#       n = n.chunk
-#     )
-#   }
-#
-# if(unlist(packageVersion("Cardinal"))[2] >= 6) {
-#   Cardinal::setCardinalNChunks(
-#     n = n.chunk
-#   )
-# }
-
-  ### Align spectra (for every pixel), filter missing or low intensity peaks
-  ### (<0.5% detected across all pixels; roughly equivalent to a peak that is detected in 10% of pixels in an individual image for
-  ### a dataset that includes 12 separate images)
-  ### Generates list of aligned m/z peaks detected for all samples
-  ### Use this output as a reference peak list for binning profile spectra to a single centroided spectra suitable for comparing across samples
-  ### Then normalize the binned spectra by sparse LOESS (sLOESS) correct for differences in intensity across sample run
-
-  ### Combine MSI data and perform peak detection
-  n.freq <- (((sum(
-    unlist(
-      lapply(
-        list.d,
-        function(x) nrow(
-          Cardinal::coord(x)
-        )
-      )
-    )
-  )/
-    length(
-      list.d
-    )))*0.1)/(
-      sum(
-        unlist(
-          lapply(
-            list.d,
-            function(x) nrow(
-              Cardinal::coord(x)
-            )
-          )
-        )
-      )
-    )
-
+  ### Combine MSI data and perform peak processing
   msi.d <- lapply(
     seq.int(
       1,
@@ -147,7 +68,7 @@ MSI.detectpeaks <- function(
     )
 
   if(unlist(packageVersion("Cardinal"))[2] < 6) {
-    d.peaks <- Cardinal::process(
+    d3 <- Cardinal::process(
       Cardinal::peakFilter(
         Cardinal::process(
           Cardinal::peakAlign(
@@ -163,23 +84,32 @@ MSI.detectpeaks <- function(
 
   if(unlist(packageVersion("Cardinal"))[2] >= 6) {
 
-    d.peaks <- Cardinal::peakAlign(
-      d,
+    d2 <- Cardinal::process(
+      Cardinal::peakPick(
+        d,
+        method="filter",
+        SNR=3
+      )
+    )
+
+    d3 <- Cardinal::peakAlign(
+      d2,
       tolerance = list.p[["resolution"]],
       units = "ppm"
     )
 
-    d.peaks <- Cardinal::subsetFeatures(
-      d.peaks,
-      freq > n.freq
-    )
+    saveRDS(
+      d3,
+      "data.aligned.rds"
+      )
   }
 
   return(
     list(
-      "Processed.Peaks" = d.peaks,
-      "num.features" = ncol(Cardinal::spectra(d.peaks))
-      ))
+      "Processed.Peaks" = d3,
+      "num.features" = nrow(Cardinal::spectra(d3))
+      )
+    )
 
   }
 
